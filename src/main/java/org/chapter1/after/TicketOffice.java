2ee47f79bd2798ad57e2af24e11ed9e74e408980
rex <rex@channel.io>
2023-02-01 23:06:22 +0900

    refator TicketOfiice code

--- src/main/java/org/chapter1/after/TicketOffice.java
+++ src/main/java/org/chapter1/after/TicketOffice.java
@@ -2,6 +2,7 @@ package org.chapter1.after;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class TicketOffice {
 
@@ -13,15 +14,14 @@ public class TicketOffice {
     this.tickets.addAll(Arrays.asList(tickets));
   }
 
-  public Ticket getTicket() {
+  public void sellTicketTo(Audience audience) {
+    plusAmount(audience.buy(getTicket()));
+  }
+  private Ticket getTicket() {
     return tickets.remove(0);
   }
 
-  public void minusAmount(long amount) {
-    this,amount -= amount;
-  }
-
-  public void plusAmount(long amount) {
+  private void plusAmount(long amount) {
     this.amount += amount;
   }
 }
